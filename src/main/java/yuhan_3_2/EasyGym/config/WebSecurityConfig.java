437bc9631b8b3d1efb5a50fac11e8b5cae467599
jhees118 <jhees118@naver.com>
2022-09-09 15:33:03 +0900

    .a

--- src/main/java/yuhan_3_2/EasyGym/config/WebSecurityConfig.java
+++ src/main/java/yuhan_3_2/EasyGym/config/WebSecurityConfig.java
@@ -1,41 +1,48 @@
 package yuhan_3_2.EasyGym.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class WebSecurityConfig{
 
     @Autowired
     private DataSource dataSource;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
-                        .antMatchers("/","/account/register").permitAll()
-                        .anyRequest().authenticated()
-                        .and()
+                .antMatchers("/","/account/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
 
                 .formLogin()
-                        .loginPage("/account/login")
-                        .permitAll()
-                        .and()
+                .loginPage("/account/login")
+                .permitAll()
+                .and()
 
                 .logout()
-                    .permitAll();
-
+                .permitAll();
+        return http.build();
     }
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth)
